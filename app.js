4e132dd7f93968fa7edacede69fd4b5c05433138
Daniel Gasienica <daniel@fiftythree.com>
2015-02-12 05:01:22 -0800

    Improve messages

--- app.js
+++ app.js
@@ -15,10 +15,21 @@ REQUIRED_ENVIRONMENT_VARIABLES.forEach(function (variable) {
 
 // Constants
 var SLACK_VERIFICATION_TOKEN = process.env.SLACK_VERIFICATION_TOKEN;
-
+var ICON_URL = 'https://www.evernote.com/shard/s1/sh/' +
+  '76f01871-af74-4a04-ba2f-4a7bc4211138/03f89a375e59f4bfe1b2a1302f97413b/' +
+  'deep/0/Mix-by-FiftyThree.png';
 
 // Helpers
-var postToSlack = function (channel, imageURL) {
+var postToSlack = function (params) {
+  var user = params.user;
+  var channel = params.channel;
+  var imageURL = params.imageURL;
+  var keyword = params.keyword;
+
+  if (typeof user !== 'string') {
+    throw new Error('`user` must be a string; got ' + channel);
+  }
+
   if (typeof channel !== 'string') {
     throw new Error('`channel` must be a string; got ' + channel);
   }
@@ -27,18 +38,33 @@ var postToSlack = function (channel, imageURL) {
     throw new Error('`imageURL` must be a string; got ' + channel);
   }
 
-  var payload = '<' + imageURL + '>';
+  if (typeof keyword !== 'string') {
+    throw new Error('`keyword` must be a string; got ' + channel);
+  }
+
+  var text = '@' + user + ' would like to share <' + imageURL + '|one of *' +
+    keyword +'’s* creations>';
+
+  if (channel === 'directmessage') {
+    channel = '@' + user;
+  } else {
+    channel = '#' + channel;
+  }
+
+  var payload = {
+    channel: channel,
+    text: text,
+    username: 'Mix',
+    icon_url: ICON_URL
+  };
 
   var options = {
     url: process.env.SLACK_WEBHOOK_URL,
     method: 'POST',
-    json: {
-      channel: '#' + channel,
-      text: payload
-    }
+    json: payload
   };
 
-  console.log('Post to Slack webhook:', options);
+  console.log('Post to Slack webhook:', payload);
   Request(options);
 };
 
@@ -90,7 +116,12 @@ app.post('/random-creation', function (req, res) {
       var randomCreation = creations[randomIndex];
       var randomImageURL = randomCreation.imageURLs['512x384/jpeg'];
 
-      postToSlack(req.body.channel_name, randomImageURL);
+      postToSlack({
+        channel: req.body.channel_name,
+        imageURL: randomImageURL,
+        keyword: user,
+        user: req.body.user_name,
+      });
       res.send(200);
   });
 });
